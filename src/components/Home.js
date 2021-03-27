c4fb451d8967723ce8431a12d94a5e6940527a32
Igor-Kazak <igor@strizhi.ru>
2021-03-27 18:47:48 +0300

    fp

---
+++ src/components/Home.js
@@ -0,0 +1,76 @@
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import TestChoose from './TestChoose'
+import img4 from '../img/airplane.png'
+import img5 from '../img/helicopter.png'
+import img4c from '../img/airplane_c.png'
+import img5c from '../img/helicopter_c.png'
+
+const Home = (props) => {
+
+    function colourImg(event) {
+        if (event.target.id === "4") {
+            event.target.src = img4c;
+        }
+        else {
+            event.target.src = img5c;
+        }
+    }
+
+    function greyImg(event) {
+        if (event.target.id === "4") {
+            event.target.src = img4;
+        }
+        else {
+            event.target.src = img5;
+        }
+    }
+
+    const { signedIn } = props;
+
+    if (signedIn) {
+        return (
+            <>
+                <TestChoose />
+            </>
+        )
+    }
+    else {
+        return (
+            <>
+                <div className="container px-5 pt-3">
+                    <div className="row gx-5 mb-3">
+                        <div className="col">
+                            <div className="p-3 px-4 border bg-light">
+                                <p className="mt-2">PPL test is the examination test for private pilots licenses, which is based on European
+                                   regulations and standards and is also ICAO-compliant.</p>
+                                <p>PPL test has two modes: test for aircraft licenses and test for helicopter licenses.</p>
+                                <div className="row gx-5 mb-0">
+                                    <div className="col tc">
+                                        <img src={img4} width="200px" alt='' id="4"
+                                        onMouseEnter={colourImg} onMouseLeave={greyImg}/>
+                                    </div>
+                                    <div className="col tc">
+                                        <img src={img5} width="200px" alt='' id="5"
+                                        onMouseEnter={colourImg} onMouseLeave={greyImg}/>
+                                    </div>
+                                </div>
+                                <p>You have 20 random questions and 30 minutes. Answer correctly at least 75% of the questions to pass the test.</p>
+                                <p>Please Log in or register for test.</p>
+                                <Link to="/signin" className="btn btn-sm btn-outline-secondary">Sign in</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        signedIn: state.reducerOne.signedIn
+    }
+}
+
+export default connect(mapStateToProps, null)(Home);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { reducer } from './redux/reducers';
+import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store} >
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Log results (for example: reportWebVitals(console.log))
+// Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
