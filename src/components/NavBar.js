986f6aa4ccbd3a60b64572fedaf976259d5c30df
Igor-Kazak <igor@strizhi.ru>
2021-03-28 22:00:16 +0300

    final project

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,13 +7,11 @@ const NavBar = (props) => {
     const { signedIn, signInSwitch, user } = props;
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light border-bottom mb-3">
+        <nav className="navbar navbar-expand navbar-light bg-light border-bottom mb-3">
             <div className="container-fluid">
                 <Link to='/' className="navbar-brand">PPL TEST</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarText">
+
+                <div className="navbar-collapse navbar-expand" id="navbarText">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link to='/' className="nav-link active" aria-current="page">Home</Link>
