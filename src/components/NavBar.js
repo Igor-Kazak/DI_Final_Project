495615b19af5e5e2a6badca1a8e1e5e569311db7
Igor-Kazak <igor@strizhi.ru>
2021-03-28 22:03:43 +0300

    final project

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,9 +14,6 @@ const NavBar = (props) => {
                 <div className="navbar-collapse navbar-expand" id="navbarText">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link to='/' className="nav-link active" aria-current="page">Home</Link>
-                        </li>
-                        <li className="nav-item">
                             <Link to='/about' className="nav-link">About</Link>
                         </li>
                         <li className="nav-item">
